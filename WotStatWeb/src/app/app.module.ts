4aafd78bafe85686822a4625b0bdbf03cec87846
Marian Stychuk <repolevedem@gmail.com>
2019-10-21 13:13:09 +0300

    Add routing

--- WotStatWeb/src/app/app.module.ts
+++ WotStatWeb/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { StatComponent } from './stat/stat.component';
+import { AppRoutingmodule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingmodule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
